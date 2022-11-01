eab063d9ddc55d7e06c68309d30a0b8ea4753189
leeseongdooo <dltjdgns3z@naver.com>
2022-11-01 18:18:53 +0900

    Day1101(1)

---
+++ src/Component/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Scss/MainPage.scss";
+
+function MainPage() {
+    return (
+        <div className="MainPageBox">
+            <div className="TextBox">
+                <span>Seong Hoon</span>
+                <span>PORTFOLIO</span>
+            </div>
+        </div>
+    )
+}
+
+export default MainPage;
