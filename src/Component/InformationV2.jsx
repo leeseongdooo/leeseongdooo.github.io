990ab15c8d3d66f2cce28a083369f46baabb425c
leeseongdooo <tjdgns8966@naver.com>
2022-12-04 02:07:55 +0900

    Day1203(2)

--- src/Component/InformationV2.jsx
+++ src/Component/InformationV2.jsx
@@ -7,33 +7,51 @@ function InformationV2() {
 
   return (
     <div className="InformationBox">
+      
       <div className="InsideBox">
-          <div className="TextArea">
-            <h2>Information</h2>
-            <p>Elasdnfjkwenqjrnjqwerqwjkern</p>
-          </div>
-
           <div className="MyInfoBox">
-            <img src={ExImage} alt="임시이미지입니다." />
+
+            <h1 >INFO</h1>
+
+            <div className="TextBox">
+              
+              <div className="Info">
+                    <img src={ProfileImage} alt="" />
+                    
+                    {/* 연락처 */}
+                    <div>
+                       <a href="">dltjdgns3z@naver.com</a>
+                       <a href="">GitHub</a>
+                       <a href="">BLOG</a>
+                    </div>
+
+              </div>
+            
+            </div>
+            {/* <p>
+                Frontend 개발자가 되고싶은 이성훈입니다. <br/>
+                우리는 바르셀로나에 기반을 두고 전 세계를 여행하며, 순수 예술을 통해 영감을 받는 자연 사진작가입니다. <br/>
+  
+                이를 바탕으로 감정적 연결을 촉진하는 사진의 역할을 탐구합니다.
+               </p> */}
+            
+            <h1>SKILL</h1>
+            
+            <div className="SkillBox">
+               <div className="Introduce">
+                  <h2>React</h2>
+                  <span>
+                    리액트 사용경험
+                  </span>
+               </div>
+
+               
+            </div>
+           
+
           </div>
 
-          <div className="ShapeBox">
-              <div className="ShapeForm">
-                  <h2>HTML</h2>
-              </div>
-
-              <div className="ShapeForm">
-                  <h2>HTML</h2>
-              </div>
-
-              <div className="ShapeForm">
-                  <h2>HTML</h2>
-              </div>
-
-              <div className="ShapeForm">
-                  <h2>HTML</h2>
-              </div>
-          </div>
+        
       </div>
     </div>
   );
