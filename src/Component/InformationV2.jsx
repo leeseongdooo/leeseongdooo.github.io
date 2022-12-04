f704fe467e512d721f65700165a22a1c4d10bb9c
leeseongdooo <tjdgns8966@naver.com>
2022-12-04 18:15:34 +0900

    Day1204(1)

--- src/Component/InformationV2.jsx
+++ src/Component/InformationV2.jsx
@@ -10,42 +10,60 @@ function InformationV2() {
       
       <div className="InsideBox">
           <div className="MyInfoBox">
-
-            <h1 >INFO</h1>
+            <h1>INFO</h1>
 
             <div className="TextBox">
-              
               <div className="Info">
-                    <img src={ProfileImage} alt="" />
-                    
+                    <img src={ExImage} alt="" />
                     {/* 연락처 */}
                     <div>
-                       <a href="">dltjdgns3z@naver.com</a>
+                       <a href="https://www.naver.com" target="_blank">dltjdgns3z@naver.com</a>
                        <a href="">GitHub</a>
                        <a href="">BLOG</a>
                     </div>
-
               </div>
-            
-            </div>
-            {/* <p>
+
+              <p>
                 Frontend 개발자가 되고싶은 이성훈입니다. <br/>
                 우리는 바르셀로나에 기반을 두고 전 세계를 여행하며, 순수 예술을 통해 영감을 받는 자연 사진작가입니다. <br/>
   
                 이를 바탕으로 감정적 연결을 촉진하는 사진의 역할을 탐구합니다.
-               </p> */}
+              </p>
+            </div>
             
-            <h1>SKILL</h1>
             
-            <div className="SkillBox">
-               <div className="Introduce">
-                  <h2>React</h2>
-                  <span>
-                    리액트 사용경험
-                  </span>
-               </div>
+            <div className="BottomBox">
+              
+               <div className="SkillBox">
+                <div className="Introduce">
+                    <h2>HTML</h2>
+                    <span>
+                      리액트 사용경험
+                    </span>
+                </div>
 
-               
+                <div className="Introduce">
+                    <h2>CSS</h2>
+                    <span>
+                      리액트 사용경험
+                    </span>
+                </div>
+
+                <div className="Introduce">
+                    <h2>JS</h2>
+                    <span>
+                      리액트 사용경험
+                    </span>
+                </div>
+
+                <div className="Introduce">
+                    <h2>React</h2>
+                    <span>
+                      리액트 사용경험
+                    </span>
+                </div>
+
+               </div>
             </div>
            
 
